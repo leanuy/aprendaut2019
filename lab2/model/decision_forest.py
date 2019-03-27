dde7ca2bba968c70f87ae853187e6503493ab8c4
gouvina <germanouvina@gmail.com>
2019-03-27 01:50:03 -0300

    Fix to forest classify

--- lab2/model/decision_forest.py
+++ lab2/model/decision_forest.py
@@ -32,22 +32,21 @@ def id3ForestClassify(forest, example, results, continuous):
     for result in results:
         clasification[result] = id3Classify(forest[result], example, continuous)
 
-    true_results = [(key, (value, probability)) for key, (value, probability) in clasification.items() if value == True]
-    if len(true_results) == 1:
-        (key, (value, probability)) = true_results[0]
+    trueResults = [(key, (value, probability)) for key, (value, probability) in clasification.items() if value == True]
+    if len(trueResults) == 1:
+        (key, (value, probability)) = trueResults[0]
         return (key, probability)
-    elif len(true_results) > 1:
-        (best_key, (best_value, best_probability)) =  max(true_results,key=itemgetter(1))
-        best_results = [(key, (value, probability)) for key, (value, probability) in clasification.items() if probability == best_probability]
-        if len(best_results) == 1:
-            (key, (value, probability)) = best_results[0]
-        elif len(best_results) > 1:
-            (key, (value, probability)) = random.choice(best_results)
+    elif len(trueResults) > 1:
+        (bestKey, (bestValue, bestProbability)) =  max(trueResults,key=itemgetter(1))
+        bestResults = [(key, (value, probability)) for key, (value, probability) in clasification.items() if probability == bestProbability]
+        if len(bestResults) == 1:
+            (key, (value, probability)) = bestResults[0]
+        elif len(bestResults) > 1:
+            (key, (value, probability)) = random.choice(bestResults)
         return (key, probability)
 
     # Nunca deberia llegar aca
-    return False
-    
+    return (random.choice(results), 0.1)    
 ### METODOS AUXILIARES
 ### -------------------
 
