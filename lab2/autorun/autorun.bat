8ec506f9057d0139e7e2b2ef723db77d736cda27
gouvina <germanouvina@gmail.com>
2019-04-02 16:50:02 -0300

    Fix: Autorun.bat has every example needed

--- lab2/autorun/autorun.bat
+++ lab2/autorun/autorun.bat
@@ -9,6 +9,24 @@ measure = 1..3
 validation = 1..2
 :EndComment
 
+python automain.py 1 1 1 1 2
+python automain.py 1 1 1 2 2
+python automain.py 1 1 1 3 2
+python automain.py 1 1 2 1 2
+python automain.py 1 1 2 2 2
+python automain.py 1 1 2 3 2
+python automain.py 1 1 3 1 2
+python automain.py 1 1 3 2 2
+python automain.py 1 1 3 3 2
+python automain.py 1 2 1 1 2
+python automain.py 1 2 1 2 2
+python automain.py 1 2 1 3 2
+python automain.py 1 2 2 1 2
+python automain.py 1 2 2 2 2
+python automain.py 1 2 2 3 2
+python automain.py 1 2 3 1 2
+python automain.py 1 2 3 2 2
+python automain.py 1 2 3 3 2
 python automain.py 2 1 1 1 1
 python automain.py 2 1 1 2 1
 python automain.py 2 1 1 3 1
