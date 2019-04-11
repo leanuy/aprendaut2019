6e87da9e154cb51c9cf094ec72515221b07030c7
gouvina <germanouvina@gmail.com>
2019-04-11 20:14:43 -0300

    Agregados atributos variables y pequeños cambios

--- lab3/model/naive_bayes.py
+++ lab3/model/naive_bayes.py
@@ -3,6 +3,7 @@
 import math
 import random
 import numpy as np
+from scipy import stats
 import processing.processor as processor
 import processing.calculator as calculator
 from utils.const import AttributeType, ContinuousOps, MeasureOps, CONTINUOUS, MEASURE
@@ -11,83 +12,102 @@ from utils.const import AttributeType, ContinuousOps, MeasureOps, CONTINUOUS, ME
 ### -------------------
 
 def nbTrain(dataset, attributes, results, options, m_est = 10):
-  # Initialization
-  classificator = {}
-  amountClass = {}
-  proportions = processor.getAllProportionExamplesForResults(dataset, results)
-  for result in results:
-    classificator[result] = {}
-    amountClass[result] = proportions[result] * len(dataset.index)
 
-    # En total se guarda la probabilidad sin considerar atributos.
-    classificator[result]['total'] = proportions[result]
+    # Inicialización
+    classificator = {}
+    amountClass = {}
+    proportions = processor.getAllProportionExamplesForResults(dataset, results)
 
-    for attribute in attributes:
-      (attributeKey, attributeType) = attribute
-      classificator[result][attributeKey] = {}
-      for value in processor.getPossibleValues(dataset, attribute):
-        classificator[result][attributeKey][value] = 0
+    for result in results:
+        classificator[result] = {}
+        amountClass[result] = proportions[result] * len(dataset.index)
 
-  # Para los atributos continuos Bayes sencillo asume distribución normal y calcula su esperanza y desviación estándar.
-  if options[CONTINUOUS] == ContinuousOps.STANDARDIZATION:
-    for attribute in attributes:
-      (attributeKey, attributeType) = attribute
-      if attributeType == AttributeType.CONTINUOUS:
-        for result in results:
-          valuesOfResult = dataset[dataset['class'] == result] # Condition: dataset['class'] == result
-          classificator[result][attributeKey]['mean'] = calculateMean(valuesOfResult, attributeKey)
-          classificator[result][attributeKey]['std_dev'] = calculateStandardDeviation(valuesOfResult, attributeKey)
+        # En total se guarda la probabilidad sin considerar atributos
+        classificator[result]['total'] = proportions[result]
 
-  # Para cada valor de cada atributo se cuenta la ocurrencia de valores.
-  for index in dataset.index:
-    result = dataset.at[index, 'class']
-    for attribute in attributes:
-      (attributeKey, attributeType) = attribute
-      value = dataset.at[index, attributeKey]
-      classificator[result][attributeKey][value] += 1      
-  
-  # Se calculan las probabilidades en base a los resultados del dataset.
-  for result in results:
-    for attribute in attributes:
-      (attributeKey, attributeType) = attribute
-      for value in classificator[result][attributeKey]:
-        if value != 'mean' and value != 'std_dev':
-          # Para cada resultado se guarda la proporcion de cada valor para cada atributo que clasificó con dicho resultado
-          classificator[result][attributeKey][value] = (classificator[result][attributeKey][value] + m_est/len(classificator[result][attributeKey])) / (amountClass[result] + m_est)
-  return classificator
+        for attribute in attributes:
+            (attributeKey, attributeType) = attribute
+            classificator[result][attributeKey] = {}
+            # Atributo continuo y estrategia de estandarización
+            if attributeType == AttributeType.CONTINUOUS and options[CONTINUOUS] == ContinuousOps.STANDARDIZATION:
+                valuesOfResult = dataset[dataset['class'] == result]
+                classificator[result][attributeKey]['mean'] = calculateMean(valuesOfResult, attributeKey)
+                classificator[result][attributeKey]['std'] = calculateStandardDeviation(valuesOfResult, attributeKey)
+            # Atributo continuo y estrategia de intervalos variables
+            elif attributeType == AttributeType.CONTINUOUS and options[CONTINUOUS] == ContinuousOps.VARIABLE:
+                for value in processor.getDiscretePossibleValues(dataset, attribute, results, options[CONTINUOUS], None):
+                    classificator[result][attributeKey][value] = 0
+            # Atributo es discreto
+            else:
+                for value in processor.getPossibleValues(dataset, attribute):
+                    classificator[result][attributeKey][value] = 0               
 
-def nbClassify(classificator, example, attributes):
-  continuousAttributes = []
-  for attribute in attributes:
-    (attributeKey, attributeType) = attribute
-    if attributeType == AttributeType.CONTINUOUS:
-      continuousAttributes.append(attributeKey)
+    # Para cada valor de cada atributo se cuenta la ocurrencia de valores
+    for index in dataset.index:
+        result = dataset.at[index, 'class']
+        for attributeKey, attributeType in attributes:
+            # Atributo continuo y estrategia de estandarización
+            if attributeType == AttributeType.CONTINUOUS and options[CONTINUOUS] == ContinuousOps.STANDARDIZATION:
+                continue
+            # Atributo continuo y estrategia de intervalos variables
+            elif attributeType == AttributeType.CONTINUOUS and options[CONTINUOUS] == ContinuousOps.VARIABLE:
+                value = dataset.at[index, attributeKey]
+                intervals = list(classificator[result][attributeKey].keys())
+                interval = processor.getIntervalForValue(value, intervals)
+                classificator[result][attributeKey][interval] += 1 
+            # Atributo es discreto
+            else:   
+                value = dataset.at[index, attributeKey]
+                classificator[result][attributeKey][value] += 1      
+    
+    # Se calculan las probabilidades en base a los resultados del dataset.
+    for result in results:
+        for attributeKey, attributeType in attributes:
+            for value in classificator[result][attributeKey]:
+                if value != 'mean' and value != 'std':
+                    # Para cada resultado se guarda la proporcion de cada valor para cada atributo que clasificó con dicho resultado
+                    classificator[result][attributeKey][value] += m_est/len(classificator[result][attributeKey])
+                    classificator[result][attributeKey][value] /= amountClass[result] + m_est
+    
+    return classificator
 
-  probabilitiesResult = {}
-  for result in classificator:
-    probabilitiesResult[result] = classificator[result]['total']
-  
-  for result in classificator:
-    for key, value in example.items():
-      if key != 'class':
-        if key in continuousAttributes:
-          probabilitiesResult[result] *= gaussianNormal(float(value), classificator[result][key]['mean'], classificator[result][key]['std_dev'])
-        else:
-          probabilitiesResult[result] *= classificator[result][key][value]
-  bestProbability = max(probabilitiesResult.values())
+def nbClassify(classificator, example, attributes, options):
+    continuousAttributes = []
+    for attributeKey, attributeType in attributes:
+        if attributeType == AttributeType.CONTINUOUS:
+            continuousAttributes.append(attributeKey)
 
-  # En caso de empate al clasificar junta todos los posibles resultados.
-  bestResults = [(i,j) for i,j in probabilitiesResult.items() if j == bestProbability]
-  return random.choice(bestResults)
+    probabilitiesResult = {}
+    for result in classificator:
+        probabilitiesResult[result] = classificator[result]['total']
+    
+    for result in classificator:
+        for key, value in example.items():
+            if key != 'class':
+                if key in continuousAttributes and options[CONTINUOUS] == ContinuousOps.STANDARDIZATION:
+                    probabilitiesResult[result] *= gaussianNormal(float(value), classificator[result][key]['mean'], classificator[result][key]['std'])
+                elif key in continuousAttributes and options[CONTINUOUS] == ContinuousOps.VARIABLE:
+                    intervals = list(classificator[result][key].keys())
+                    interval = processor.getIntervalForValue(value, intervals)
+                    probabilitiesResult[result] *= classificator[result][key][interval]
+                else:
+                    probabilitiesResult[result] *= classificator[result][key][value]
+
+    bestProbability = max(probabilitiesResult.values())
+
+    # En caso de empate al clasificar junta todos los posibles resultados.
+    bestResults = [(i,j) for i,j in probabilitiesResult.items() if j == bestProbability]
+    return random.choice(bestResults)
 
 ### METODOS AUXILIARES
 ### -------------------
 
 def calculateMean(valuesOfResult, attribute):
-  return np.mean(valuesOfResult[attribute], axis=0)
+    return np.mean(valuesOfResult[attribute], axis=0)
 
 def calculateStandardDeviation(valuesOfResult, attribute):
-  return np.std(valuesOfResult[attribute], axis=0)
+    return np.std(valuesOfResult[attribute], axis=0)
 
-def gaussianNormal(value, mean, std_dev):
-  return math.exp(-((value-mean)/(2*std_dev))**2)/(math.sqrt(2*math.pi*std_dev**2))
+def gaussianNormal(value, mean, std):
+    return 1 if std == 0 else stats.norm(mean, std).pdf(value)
+    #return math.exp(-((value-mean)/(2*std_dev))**2)/(math.sqrt(2*math.pi*std_dev**2))
