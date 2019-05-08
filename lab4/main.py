9b2877b5f504b619d42c337b1dbce05ce5660a3d
Lenny <ldrnuy@gmail.com>
2019-05-07 22:58:55 -0300

    aqui vamos

--- lab4/main.py
+++ lab4/main.py
@@ -8,18 +8,12 @@ import time
 from model import pca, k_means
 import processing.reader as reader
 import processing.parser as parser
-import processing.processor as processor
 import plotting.pcaPlotting as pcaPlotting
 import plotting.kMeansPlotting as kMeansPlotting
 import utils.gui as gui
-<<<<<<< HEAD
 from utils.const import DATA_ENCUESTAS, DATA_CANDIDATOS, MenuOps
 from sklearn.metrics import silhouette_score
 from sklearn.metrics.cluster import adjusted_rand_score
-=======
-from utils.const import DATA_ENCUESTAS, DATA_CANDIDATOS, MenuOps, PCAOps
-from sklearn.metrics import silhouette_samples, silhouette_score
->>>>>>> 6d8fb6c83ac68cbfc555d5fc3e26676dc4aa8d2e
 
 ### METODO PRINCIPAL
 ### ----------------
@@ -55,6 +49,7 @@ if __name__ == '__main__':
             pcaPlotting.plotPCA(reducedDataset, candidates, options, extras)
 
         elif op == MenuOps.KMEANS:
+
             # Leer K
             K = gui.printModelK(op)
 
@@ -66,23 +61,17 @@ if __name__ == '__main__':
 
             # Calcular el avg del silhouette de todas las filas
             cluster_labels = dataset_classified[:, 26]
-            silhouette_avg = silhouette_score(dataset.values, cluster_labels)
+            silhouette_avg = silhouette_score(dataset.values, cluster_labels, sample_size=22500)
             print("silhouette avg: ", silhouette_avg)
 
-            partyJSON = reader.readParties(DATA_CANDIDATOS)
-            parsedParties, parsedCandidates = parser.parseCandidates(candidates.values, partyJSON)
-
-            # Calculamos ARI sólo para 11 clusters
-            if K == 11:
-                # Calcular el Adjusted Rand Index
-                # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.adjusted_rand_score.html
-                labels_true = []
-                for index in dataset.index:
-                    labels_true.append(processor.getParty(parsedParties, candidates[index]))
-                ari = adjusted_rand_score(labels_true, cluster_labels)
-                print("ARI: ", ari)
+            # Calcular el Adjusted Rand Index
+            # link https://scikit-learn.org/stable/modules/generated/sklearn.metrics.adjusted_rand_score.html
+            # Cambiar el valor de labels_true a lo que corresponda.
+            labels_true = cluster_labels
+            ari = adjusted_rand_score(labels_true, cluster_labels)
+            print("ARI: ", ari)
 
             # Plotting
-            kMeansPlotting.plotKMeansParties(dataset, candidates, parsedParties, centroids, classes)
+            kMeansPlotting.plotKMeansParties(dataset, candidates, centroids, classes)
 
         input("-> Oprima enter para volver al menú")
